8ff7c1307bf4b2e5480fccf4462188aca0d5f136
jet-isnt-haha <760181734@qq.com>
2025-05-24 17:16:18 +0800

    mobile-initialize

---
+++ the-language-of-the-part-time-worker/mobile/apps/appButton3.gd
@@ -0,0 +1,9 @@
+extends Button
+
+func _ready():
+	self.pressed.connect(_on_button_pressed)
+	
+func _on_button_pressed():
+		var scene = load("res://mobile/mobile.tscn")
+		get_tree().change_scene_to_packed(scene)
+		
