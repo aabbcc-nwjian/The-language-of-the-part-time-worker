5a0e660f175e29c8f10d8e2ad6c96f95ba35342f
jet-isnt-haha <760181734@qq.com>
2025-05-25 02:25:23 +0800

    new file:   the-language-of-the-part-time-worker/assets/appbank-bar.png         new file:   the-language-of-the-part-time-worker/assets/appbank-bar.png.import         new file:   the-language-of-the-part-time-worker/assets/appbank.png         new file:   the-language-of-the-part-time-worker/assets/appbank.png.import         modified:   the-language-of-the-part-time-worker/scene/appbank_scene.tscn         modified:   the-language-of-the-part-time-worker/script/appbank_scene.gd

--- the-language-of-the-part-time-worker/script/appbank_scene.gd
+++ the-language-of-the-part-time-worker/script/appbank_scene.gd
@@ -7,13 +7,13 @@ var balance: int = 0
 
 func _ready() -> void:
 	balance=User.money
-	balance_label.text = '当前余额：'+str(balance)
+	balance_label.text = str(balance)
 	
 
 
 
 func _update_money() -> void:
-	balance_label.text = '当前余额：'+str(User.money)
+	balance_label.text =str(User.money)
 	pass
 
 
@@ -24,21 +24,26 @@ func _on_accept_dialog_confirmed(loan:int) -> void:
 	loan = 0
 
 
-func _on_loan_pressed() -> void:
-	loan = 0
-	accept_dialog.popup()
-
-
 func _on_loan_1000_pressed() -> void:
 	loan= 1000
-	_on_accept_dialog_confirmed(loan)
-
 
 
 func _on_loan_2000_pressed() -> void:
 	loan = 2000
-	_on_accept_dialog_confirmed(loan)
 
 func _on_loan_3000_pressed() -> void:
 	loan= 3000
+
+
+func _on_loan_4000_pressed() -> void:
+	loan= 4000
+
+
+func _on_back_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_confirm_pressed() -> void:
+	accept_dialog.popup()
 	_on_accept_dialog_confirmed(loan)
+	pass # Replace with function body.
